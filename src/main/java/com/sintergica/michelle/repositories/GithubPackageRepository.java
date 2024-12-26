7334209b5bb21dae7a1cf6b20113547ca7171780
edsonmanuelcarballovera <edsonmanuelcarballovera@gmail.com>
2024-12-26 13:13:35 -0600

    fixed bug that prevented from loading all the tags from a package on ghcr

--- src/main/java/com/sintergica/michelle/repositories/GithubPackageRepository.java
+++ src/main/java/com/sintergica/michelle/repositories/GithubPackageRepository.java
@@ -7,6 +7,7 @@ import okhttp3.Response;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,9 +23,11 @@ public class GithubPackageRepository {
 			+ "/packages?package_type=" + PACKAGE_TYPE_CONTAINER;
 	}
 
-	private String buildVersionUrl(String packageName) {
+	private String buildVersionUrl(String packageName, int page) {
 		return githubHttpClient.getBaseUrl()
-			+ "/packages/" + PACKAGE_TYPE_CONTAINER + "/" + packageName + "/versions";
+			+ "/packages/" + PACKAGE_TYPE_CONTAINER + "/" + packageName
+			+ "/versions?per_page=100"
+			+ "&page=" + page;
 	}
 
 	public List<Package> getPackages() {
@@ -43,9 +46,9 @@ public class GithubPackageRepository {
 		}
 	}
 
-	public List<String> getPackageVersions(String packageName) {
+	public List<String> getPackageVersions(String packageName, int page) {
 		try {
-			Response response = githubHttpClient.executeRequest(buildVersionUrl(packageName));
+			Response response = githubHttpClient.executeRequest(buildVersionUrl(packageName, page));
 			return response != null
 				? githubHttpClient.extractTagsFromResponse(response)
 				: Collections.emptyList();
@@ -55,9 +58,14 @@ public class GithubPackageRepository {
 	}
 
 	public void addTagsToPackage(Package _package) {
-		List<String> versions = getPackageVersions(_package.getName());
-		if (!versions.isEmpty()) {
-			_package.setTags(versions);
+		List<String> tags = new ArrayList<>();
+		int page = 1;
+		List<String> packageVersions = getPackageVersions(_package.getName(), page);
+		while (!packageVersions.isEmpty()) {
+			page++;
+			tags.addAll(packageVersions);
+			packageVersions = getPackageVersions(_package.getName(), page);
 		}
+		_package.setTags(tags);
 	}
 }
